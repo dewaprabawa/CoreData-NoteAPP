a55c7b0abe5d305dd2fa6da6ebd2615f5fc36762
dewa <dewaprabawa>
2020-09-29 19:04:40 +0800

    first commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Dewa Prabawa on 29/09/20.
+//  Copyright © 2020 Dewa Prabawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
